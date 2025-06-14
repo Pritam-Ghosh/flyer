c4fcf17bd4748ccdc69fa1f91ae933bb21d55a4f
Pritam <pritam7599@gmail.com>
2025-06-14 16:22:33 +0530

    flyer

---
+++ src/app/component/Template.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Template() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <input type="text" placeholder="search" />
+                </li>
+                <li>
+                    <nav>
+                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+                        </div>
+                    </nav>
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+                        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+                    </div>
+                </li>
+            </ul>
+
+        </>
+    )
+}
+
+export default Template
